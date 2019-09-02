2caf42659d687e1e9ed219aa570acfcb6f123694
nonex <nonex@Nonex-DEV>
2019-09-02 14:27:40 -0500

    keywords y demas metas

--- resources/views/Tienda/servicio.blade.php
+++ resources/views/Tienda/servicio.blade.php
@@ -1,4 +1,10 @@
 @extends('principal')
+@section('keywords')
+	<meta name="keywords" content="{{$servicio->meta_keywords}}">
+	<title>{{$servicio->meta_title}}</title>
+	<meta name="description" content="{{$servicio->meta_description}}">
+
+@endsection
 @section('content')
 	<div class="col-lg-9" style="margin-top: 5%;">
 		
